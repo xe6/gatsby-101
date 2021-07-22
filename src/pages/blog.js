ac963a0b54d439449e2377b8731359b58ad5d1f3
Max K <max@ezic.io>
2021-07-22 19:07:31 +0300

    render mdx blog posts

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,27 +1,36 @@
 import * as React from "react";
 import { graphql } from "gatsby";
+import { MDXRenderer } from 'gatsby-plugin-mdx'
 import Layout from "../components/layout";
 
 const BlogPage = ({ data }) => {
     return (
         <Layout pageTitle="My Blog Posts">
             <ul>
-                {data.allFile.nodes
-                    .filter((n) => n.extension === "mdx")
-                    .map((n) => (
-                        <li key={n.name}>{n.name}</li>
-                    ))}
+                {data.allMdx.nodes.map((node) => (
+                    <article key={node.id}>
+                        <h2>{node.frontmatter.title}</h2>
+                        <p>Posted: {node.frontmatter.date}</p>
+                        <MDXRenderer>
+                            {node.body}
+                        </MDXRenderer>
+                    </article>
+                ))}
             </ul>
         </Layout>
     );
 };
 
 export const query = graphql`
-    query {
-        allFile {
+    {
+        allMdx(sort: { fields: frontmatter___date, order: DESC }) {
             nodes {
-                name
-                extension
+                frontmatter {
+                    date(formatString: "MMMM D, YYYY")
+                    title
+                }
+                id
+                body
             }
         }
     }
